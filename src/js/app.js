98e252d026319302b5dc20de05d1538edee53bdb
RegularSvensson <elias.svensson.1992@gmail.com>
2015-06-03 14:56:07 +0200

    Files setup

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+define(
+  [
+    'angular',
+    'angular-route',
+    'js/controller/home-controller',
+    'js/controller/about-controller'
+  ],
+  function(angular) {
+    'use strict';
+
+    angular
+      .module('ng-boilerplate', ['ngRoute', 'ng-boilerplate.home-controller'])
+      .config([
+        '$routeProvider',
+        '$sceProvider',
+        '$locationProvider',
+        function($routeProvider, $sceProvider, $locationProvider) { // jshint ignore:line
+          $routeProvider
+            .when('/', {
+              controller: 'HomeController',
+              templateUrl: 'public/template/home.html'
+            })
+            .when('/about', {
+              controller: 'AboutController',
+              templateUrl: 'public/template/about.html'
+            })
+            .otherwise({ redirectTo: '/' });
+
+          // Disables Strict Contextual Escaping for IE8 compatibility
+          $sceProvider.enabled(false);
+
+//          // Commented out for use on GitHub Pages
+//          // Only use html5Mode for modern browsers
+//          if (window.history && history.pushState) {
+//            $locationProvider.html5Mode(true);
+//          }
+        }
+      ]);
+  }
+);
---
+++ src/js/controller/about-controller.js
@@ -0,0 +1,18 @@
+define(
+  [
+    'angular',
+  ],
+  function(angular) {
+    'use strict';
+
+    angular
+      .module('ng-boilerplate.about-controller', [
+      ])
+      .controller('AboutController', [
+        '$scope',
+        function($scope) {
+          
+        }
+      ]);
+  }
+);
